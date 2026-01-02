9ef80afd7d6b99ba6d0c79bf6bb4ee3dfde81e9d
Zavodny Petr <petr.zavodny@sensearena.com>
2026-01-02 10:02:10 +0100

    Basic layout

---
+++ src/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+﻿import styles from './Sidebar.module.scss';
+
+function Sidebar() {
+    return (
+        <div className={styles.container}>
+            <h2>Navigation</h2>
+        </div>
+    )
+}
+
+export default Sidebar;
